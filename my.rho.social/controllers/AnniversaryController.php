379e100962b8d7c196d8bef606d399ff2a559fb8
ceciliazhao <cecilia@vistart.name>
2016-02-03 14:39:28 +0800

    add controllers
    add AddressController,AnniversaryController,UrlController and
    IMController

---
+++ my.rho.social/controllers/AnniversaryController.php
@@ -0,0 +1,108 @@
+﻿<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rho_my\controllers;
+
+use Yii;
+use common\models\user\contact\Anniversary;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ *
+ * @author vistart <i@vistart.name>
+ */
+final class AnniversaryController extends DefaultController
+{
+
+    use ContactTrait;
+    const SESSKEY_MY_ANNIVERSARY = 'sesskey_my_anniversary';
+
+    public $layout = 'anniversary/main';
+
+    public function actionIndex()
+    {
+        return $this->render('index', ['newModel' => static::getIdentityNewModel()]);
+    }
+
+    public static function getIdentityNewModel()
+    {
+        $identity = Yii::$app->user->identity;
+        $model = $identity->create(Anniversary::className());
+        $model->loadDefaultValues();
+        return $model;
+    }
+
+    public function actionNew()
+    {
+        $result = static::insertItem(Anniversary::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_ANNIVERSARY, $result);
+        return $this->redirect(['anniversary/index']);
+    }
+
+    public function actionUpdate($id)
+    {
+        $result = static::updateItem($id, Anniversary::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_ANNIVERSARY, $result);
+        return $this->redirect(['anniversary/index']);
+    }
+
+    public function actionDelete($id)
+    {
+        $result = static::deleteItem($id, Anniversary::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_ANNIVERSARY, $result);
+        return $this->redirect(['anniversary/index']);
+    }
+
+    public function actionGets($list = 0)
+    {
+        if ($list) {
+            return static::getCountJson(Anniversary::className());
+        }
+        return static::getItem(Anniversary::className());
+    }
+
+    public function actionGet($id)
+    {
+        return static::getModelWidget($id, Anniversary::className());
+    }
+
+    public static function getFlash()
+    {
+        return static::getFlashNotifification(static::SESSKEY_MY_ANNIVERSARY);
+    }
+
+    public static function getRouteGet()
+    {
+        return 'anniversary/get';
+    }
+
+    public static function getRouteGets()
+    {
+        return 'anniversary/gets';
+    }
+
+    public static function getRouteNew()
+    {
+        return 'anniversary/new';
+    }
+
+    public static function getRouteUpdate()
+    {
+        return 'anniversary/update';
+    }
+
+    public static function getRouteDelete()
+    {
+        return 'anniversary/delete';
+    }
+}
---
+++ my.rho.social/controllers/UrlController.php
@@ -0,0 +1,108 @@
+﻿<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016 vistart
+ * @license https://vistart.name/license/
+ */
+
+namespace rho_my\controllers;
+
+use Yii;
+use common\models\user\contact\Url;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ *
+ * @author vistart <i@vistart.name>
+ */
+final class UrlController extends DefaultController
+{
+
+    use ContactTrait;
+    const SESSKEY_MY_URL = 'sesskey_my_url';
+
+    public $layout = 'url/main';
+
+    public function actionIndex()
+    {
+        return $this->render('index', ['newModel' => static::getIdentityNewModel()]);
+    }
+
+    public static function getIdentityNewModel()
+    {
+        $identity = Yii::$app->user->identity;
+        $model = $identity->create(Url::className());
+        $model->loadDefaultValues();
+        return $model;
+    }
+
+    public function actionNew()
+    {
+        $result = static::insertItem(Url::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_URL, $result);
+        return $this->redirect(['url/index']);
+    }
+
+    public function actionUpdate($id)
+    {
+        $result = static::updateItem($id, Url::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_URL, $result);
+        return $this->redirect(['url/index']);
+    }
+
+    public function actionDelete($id)
+    {
+        $result = static::deleteItem($id, Url::className());
+        static::setFlashNotificationByResult(static::SESSKEY_MY_URL, $result);
+        return $this->redirect(['url/index']);
+    }
+
+    public function actionGets($list = 0)
+    {
+        if ($list) {
+            return static::getCountJson(Url::className());
+        }
+        return static::getItem(Url::className());
+    }
+
+    public function actionGet($id)
+    {
+        return static::getModelWidget($id, Url::className());
+    }
+
+    public static function getFlash()
+    {
+        return static::getFlashNotifification(static::SESSKEY_MY_URL);
+    }
+
+    public static function getRouteGet()
+    {
+        return 'url/get';
+    }
+
+    public static function getRouteGets()
+    {
+        return 'url/gets';
+    }
+
+    public static function getRouteNew()
+    {
+        return 'url/new';
+    }
+
+    public static function getRouteUpdate()
+    {
+        return 'url/update';
+    }
+
+    public static function getRouteDelete()
+    {
+        return 'url/delete';
+    }
+}
