bbece739007a2e251b24bb0e915877007c656796
ceciliazhao <cecilia@vistart.name>
2016-02-03 21:22:31 +0800

    add _list view
    add _list view of address, anniversary, email, im and list

---
+++ my.rho.social/views/email/_list.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link http://vistart.name/
+ * @copyright Copyright (c) 2016 vistart
+ * @license http://vistart.name/license/
+ */
+use rho_my\controllers\EmailController as ec;
+use rho_my\widgets\item\ItemListWidget as il;
+
+/* @var $list rho_my\widgets\item\ItemListWidget */
+?>
+<?= il::widget(['getItemUrl' => ec::getRouteGets(), 'getCountUrl' => [ec::getRouteGets(), 'list' => 1]]); ?>
