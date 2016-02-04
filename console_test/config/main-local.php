dfed65082b8026a09e3b2dddfbb22d12a074fcd9
Zhao vistart <i@vistart.name>
2016-02-04 10:56:25 +0800

    attach db component.

---
+++ console_test/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016 vistart
+ * @license https://vistart.name/license/
+ */
+
+return [
+    'components' => [
+        'db' => require (__DIR__ . '/../../common/config/db/db-local.php'),
+    ],
+];
