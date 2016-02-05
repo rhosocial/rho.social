60f4222cc824122f8b834b644b02bcf091835635
Zhao vistart <i@vistart.name>
2016-02-05 14:27:24 +0800

    remove redundant cnzzCode

--- my.rho.social/views/layouts/main.php
+++ my.rho.social/views/layouts/main.php
@@ -15,11 +15,6 @@ use common\widgets\FooterWidget;
 CommonAsset::register($this);
 require('topbar-second.php');
 ?>
-<?php
-$cnzzCode = <<<EOT
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1255444435'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1255444435' type='text/javascript'%3E%3C/script%3E"));</script>
-EOT;
-?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
